d26aa51ffd315291e14e50d12b38c32b83244076
Henrique Ernandes Rebelo <61748405+henriquernandes@users.noreply.github.com>
2021-09-09 22:00:19 -0300

    new commit

--- index.js
+++ index.js
@@ -1,135 +1,104 @@
-const dotenv = require('dotenv');
-const fetch = require('node-fetch');
-const { Client, Intents, MessageEmbed } = require('discord.js');
+const dotenv = require("dotenv");
+const fetch = require("node-fetch");
+const { Client, Intents, MessageEmbed } = require("discord.js");
 dotenv.config();
 
-let currency = ['usd', 'brl']
-let current = 0
-let reply = ''
-let dolarUpdated = '';
+let currency = ["usd", "brl"];
+let current = 0;
+let reply = "";
+let dolarUpdated = "";
+let result;
 let dolar = 0;
 let real = 0;
-let coins = {
-	'slp': 'smooth-love-potion',
-	'btc': 'bitcoin',
-	'pvu': 'plant-vs-undead-token',
-	'pmon': 'polychain-monsters',
-	'ccar': 'cryptocars'
-}
- 
+
 //Get crypto price via Gecko API
 async function priceJSON(z) {
-		const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency='+ currency[current] +'&ids=' + coins[z]).catch((err) => {
-		console.log('API call error:', err.message);
-		});
-		const price = await response.json();
-		let result
-		if(current == 0){
-		 result = 'Current price: U$ ' + price[0].current_price + ' for ' + price[0].name
-		}
-		if(current == 1){
-			result = 'Current price: R$ ' + price[0].current_price + ' for ' + price[0].name
-		   }
-		return result
+  const response = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + currency[current] + '&symbols=' + z
+  ).catch((err) => {console.log("API call error:", err.message);});
+  if(response != '' && response != undefined) {
+	const price = await response.json();
+	result = price[0]
+	console.log(price[0])
+	return price[0].symbol;
+  }
 }
 
 async function getDolar() {
-	while(current < 1){
-		let response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency='+ currency[current] +'&ids=polychain-monsters').catch((err) => {
-		console.log('API call error:', err.message);
-		});
-		let price = await response.json();
-		dolar = price[0].current_price
-		current++
-	}	
-	let response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency='+ currency[current] +'&ids=polychain-monsters').catch((err) => {
-	console.log('API call error:', err.message);
-	});
-	let price = await response.json();
-	real = price[0].current_price
-	let result = real / dolar
-	dolarUpdated = 'R$ ' + result.toFixed('2')
-	current = 0
-	return dolarUpdated
+  while (current < 1) {
+    let response = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" +
+        currency[current] +
+        "&ids=polychain-monsters"
+    ).catch((err) => {
+      console.log("API call error:", err.message);
+    });
+    let price = await response.json();
+    dolar = price[0].current_price;
+    current++;
+  }
+  let response = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" +
+      currency[current] +
+      "&ids=polychain-monsters"
+  ).catch((err) => {
+    console.log("API call error:", err.message);
+  });
+  let price = await response.json();
+  real = price[0].current_price;
+  let result = real / dolar;
+  dolarUpdated = "R$ " + result.toFixed("2");
+  current = 0;
+  return dolarUpdated;
 }
 
 // Create a new client instance
-const client = new Client({ 
-		intents: [
-			Intents.FLAGS.GUILDS,
-			Intents.FLAGS.GUILD_MESSAGES
-		] 
-	});
-
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on('messageCreate', 	async (message) => {
-	if(message.content === 'comandos'){
-		message.reply({
-			content: 'Comandos disponiveis para consulta de valores: slp pvu pmon ccar btc dolar'
-		})
-	}
-	if(message.content === 'slp'){
-		reply = await priceJSON(message.content) + '\n'
-		current++
-		reply += await priceJSON(message.content)
-		current--
-		message.reply({
-			content: reply
-		})
-	}
-	if(message.content === 'btc'){
-		reply = await priceJSON(message.content) + '\n'
-		current++
-		reply += await priceJSON(message.content)
-		current--
-		message.reply({
-			content: reply
-		})
-	}
-	if(message.content === 'pvu'){
-		reply = await priceJSON(message.content) + '\n'
-		current++
-		reply += await priceJSON(message.content)
-		current--
-		message.reply({
-			content: reply
-		})
-	}
-	if(message.content === 'pmon'){
-		reply = await priceJSON(message.content) + '\n'
-		current++
-		reply += await priceJSON(message.content)
-		current--
-		message.reply({
-			content: reply
-		})
-	}
-	if(message.content === 'ccar'){
-		reply = await priceJSON(message.content) + '\n'
-		current++
-		reply += await priceJSON(message.content)
-		current--
-		message.reply({
-			content: reply
-		})
-	}
-	if(message.content === 'dolar'){
-		dolarUpdated = await getDolar()
-		message.reply({
-			content: dolarUpdated
-		})
-	}
-	if(message.content === 'pizza'){
-		message.reply({
-			content: 'https://i.pinimg.com/originals/36/ce/79/36ce7968e79e2af080a13e8f20895e97.jpg'
-		})
-	}
-})
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.content === "comando") {
+    message.reply({
+      content:
+        "Comandos disponiveis para consulta de valores: " + coins,
+    });
+  }
+  if (message.content == await priceJSON(message.content)) {
+    message.reply({
+      content: 'U$ ' + result.current_price + ' for ' + result.name
+    });
+  }
+  if (message.content === "!change dolar") {
+    current = 1
+    message.reply({
+      content: 'Currency updated'
+    });
+  }
+  if (message.content === "!change real") {
+    current = 0
+    message.reply({
+      content: 'Currency updated'
+    });
+  }
+  if (message.content === "dolar") {
+    dolarUpdated = await getDolar();
+    message.reply({
+      content: dolarUpdated,
+    });
+  }
+  if (message.content === "pizza") {
+    message.reply({
+      content:
+        "https://i.pinimg.com/originals/36/ce/79/36ce7968e79e2af080a13e8f20895e97.jpg",
+    });
+  }
+});
 
 // Login to Discord with your client's token
 client.login(process.env.TOKEN);
-
