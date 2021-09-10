a7887379b29a38cc1cb32a44bc1e46faf1c41f2a
Henrique Ernandes Rebelo <61748405+henriquernandes@users.noreply.github.com>
2021-09-09 23:05:48 -0300

    Alteração para comandos com exclamação

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const fetch = require("node-fetch");
 const { Client, Intents, MessageEmbed } = require("discord.js");
 dotenv.config();
 
-let currency = ["usd", "brl"];
+const prefix = '!'
+let currency = ["brl", "usd"];
+let symbolCurr = ['R$', 'U$']
 let current = 0;
 let reply = "";
 let dolarUpdated = "";
@@ -16,11 +18,10 @@ async function priceJSON(z) {
   const response = await fetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + currency[current] + '&symbols=' + z
   ).catch((err) => {console.log("API call error:", err.message);});
-  if(response != '' && response != undefined) {
 	const price = await response.json();
-	result = price[0]
-	console.log(price[0])
-	return price[0].symbol;
+  if(price != '' && price != undefined) {
+	  result = price[0]
+	  return price[0].symbol;
   }
 }
 
@@ -63,41 +64,51 @@ client.once("ready", () => {
 });
 
 client.on("messageCreate", async (message) => {
-  if (message.content === "comando") {
+  if (message.content === "!v") {
     message.reply({
       content:
         "Comandos disponiveis para consulta de valores: " + coins,
     });
   }
-  if (message.content == await priceJSON(message.content)) {
-    message.reply({
-      content: 'U$ ' + result.current_price + ' for ' + result.name
+  if (!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLocaleLowerCase()
+
+    if (command === 'v') {
+      if(!args.length) return
+      else if(await priceJSON(args[0])) {
+        if(args[1] != '' && args[1] != undefined){
+          message.reply({
+            content: symbolCurr[current] + ' ' + (result.current_price * args[1]).toFixed(6) + ' for ' + result.name
+        });}
+        else {
+          message.reply({
+            content: symbolCurr[current] + ' ' +  result.current_price + ' for ' + result.name
+        });
+      }
+    }
+    else if(args[0] === 'real'){
+      current = 0
+      return message.channel.send('Currency update to ' + symbolCurr[current])
+    }
+    else if(args[0] === 'dolar'){
+      current = 1
+      return message.channel.send('Currency update to ' + symbolCurr[current])
+    }
+    else if (args[0] === "easteregg") {
+      const user = message.mentions.users.first();
+      message.channel.send({
+        content:"Bora comer uma \n https://i.pinimg.com/originals/36/ce/79/36ce7968e79e2af080a13e8f20895e97.jpg",
+      });
+    }
+    else{
+      message.reply({
+        content: 'Crypto not found or command invalid'
     });
+    }
   }
-  if (message.content === "!change dolar") {
-    current = 1
-    message.reply({
-      content: 'Currency updated'
-    });
-  }
-  if (message.content === "!change real") {
-    current = 0
-    message.reply({
-      content: 'Currency updated'
-    });
-  }
-  if (message.content === "dolar") {
-    dolarUpdated = await getDolar();
-    message.reply({
-      content: dolarUpdated,
-    });
-  }
-  if (message.content === "pizza") {
-    message.reply({
-      content:
-        "https://i.pinimg.com/originals/36/ce/79/36ce7968e79e2af080a13e8f20895e97.jpg",
-    });
-  }
+  
 });
 
 // Login to Discord with your client's token
